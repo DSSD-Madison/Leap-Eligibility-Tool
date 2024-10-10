080a38a5beafe3b87cd9687c89693e00b0aa239e
Sarah <sarah.i.rubenstein@gmail.com>
2024-10-10 13:58:30 -0500

    Home page added, router moved to App

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,22 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
 import './index.css'
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <div>Page not found</div>,
-  },
-]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 )
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+function Home() {
+    // testing branch
+    return (
+      <p>Home</p>
+    )
+  }
+  
+  export default Home
