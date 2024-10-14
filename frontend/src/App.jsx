3e3a984efd9dd43d70855f03029e5c08a1cb7bba
Ashwin Talwalkar <atalwalkar719@gmail.com>
2024-10-13 19:46:05 -0500

    fixed routers logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,18 +5,25 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
-const router = createBrowserRouter([
+const allRoutes = [
   {
     path: "/",
     element: <Home />,
     errorElement: <p>Page not found</p>,
   },
-]);
+];
+
+const router = createBrowserRouter(
+  allRoutes.map((route) => ({
+    ...route,
+    element: <Layout>{route.element}</Layout>,
+  }))
+);
 
 function App() {
   // testing branch
   return (
-    <RouterProvider router={router} />
+    <RouterProvider router={router} />  
   )
 }
 
